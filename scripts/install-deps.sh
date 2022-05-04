a2b3f7e35eb6492f2d40d0c53e03f430da8653d3
Mark Gutenberger <Mark-Gutenberger@outlook.com>
2022-05-04 16:44:16 -0500

    add dirent

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -3,7 +3,7 @@
 # install-deps.sh (c) 2022
 # Desc: description
 # Created:  2022-05-03T18:56:11.669Z
-# Modified: 2022-05-04T21:26:24.189Z
+# Modified: 2022-05-04T21:38:37.302Z
 
 cd lib
 rm ./* -rf
@@ -11,4 +11,6 @@ curl -R -O https://www.lua.org/ftp/lua-5.4.4.tar.gz
 tar -xf ./lua-5.4.4.tar.gz
 curl -R https://codeload.github.com/fmtlib/fmt/tar.gz/refs/tags/8.1.1 -o ./fmt-8.1.1.tar.gz
 tar -xf ./fmt-8.1.1.tar.gz
+curl -R https://codeload.github.com/tronkko/dirent/tar.gz/refs/tags/1.23.2 -o ./dirent-1.23.2.tar.gz
+tar -xf ./dirent-1.23.2.tar.gz
 exit 0
--- src/core.h
+++ src/core.h
@@ -16,4 +16,6 @@ Modified:2022-05-03T19:06:40.400Z
 #include <iostream>
 #include <string>
 
+#include "../lib/dirent-1.23.2/include/dirent.h"
+#include "../lib/fmt-8.1.1/include/fmt/core.h"
 #include "../lib/lua-5.4.4/src/lua.hpp"
