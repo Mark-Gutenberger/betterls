4f63096e5a3defc918a347da8c65cd06b506b836
mark gutenberger <mark-gutenberger@outlook.com>
2022-06-04 22:48:45 -0500

    work on lame README

---
+++ CONTRIBUTORS.md
@@ -0,0 +1,7 @@
+## Contributors
+
+(in order of addition)
+
+---
+
+Mark Gutenberger <[mark-gutenberger@outlook.com](mailto:mark-gutenberger@outlook.com)> ***(Owner)***
---
+++ src/config.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include "../include/rapidjson/document.h"		// rapidjson's DOM-style API
+#include "../include/rapidjson/prettywriter.h"	// for stringify JSON
+namespace betterls {
+class Config {
+	void mom() {
+		std::ifstream myfile;
+		std::string testfile = "test.json";
+		myfile.open(testfile);
+		std::string myline;
+		if (myfile.is_open()) {
+			// meta equivalent to of `for lines in file`
+			while (myfile) {
+				std::getline(myfile, myline);
+				std::cout << myline << '\n';
+			}
+		} else {
+			std::cout << "Couldn't open file\n";
+			std::cout << "that's okay, make a file `test.json` in the locatino you run this executable.\n";
+		}
+	};
+};
+};	// namespace betterls
